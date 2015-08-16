52e7dc3fdfd8e9d2795dc10ee6db5707a252c9bd
tangsulong.home <tangsulong@51offer.me>
2015-08-16 23:53:56 +0800

    add github

--- src/controller/controller.go
+++ src/controller/controller.go
@@ -3,17 +3,18 @@ package controller
 import (
 	"download"
 	"extractor"
-	//"fmt"
+	"fmt"
 	"model"
 	"utils"
 )
 
 func CrawlController() {
-
+	fmt.Println(" CrawlController ")
 	var crawlUrl model.CrawlURL = model.CrawlURL{}
 	crawlUrl.Url = utils.URL_SEED
-	webEntity := download.Download(&crawlUrl)
-	extractor.Extract(webEntity)
+	//webEntity := download.Download(&crawlUrl)
+	//extractor.Extract(webEntity)
+	extractor.ExtractHtml(&crawlUrl)
 	//fmt.Println("web content : ", *webEntity.Body)
 
 }
--- src/extractor/extractor.go
+++ src/extractor/extractor.go
@@ -12,6 +12,7 @@ func Extract(webEntity *model.WebEntity) {
 	}
 	html := *webEntity.Body
 	nodes, err := goquery.ParseString(html)
+
 	if err != nil {
 		fmt.Println("goquery ParseString error : ", err)
 		panic(err)
@@ -19,4 +20,25 @@ func Extract(webEntity *model.WebEntity) {
 		title := nodes.Find("title").Text()
 		fmt.Println("title : ", title)
 	}
+}
+
+func ExtractHtml(crawlUrl *model.CrawlURL) {
+	fmt.Println(">>> ExtractHtml ... ")
+	var doc *Document
+	var e error
+	if doc, e = NewDocument(crawlUrl.Url); e != nil {
+		panic(e.Error())
+	}
+
+	doc.Find("tr").Each(func(i int, ss *goquery.Selection) {
+		s := *ss.Find("td")
+		if s.Length == 6 {
+			port, _ := strconv.Atoi(s.Eq(1).Text())
+			if port > 1 {
+				//var pInfo ProxyInfo
+				fmt.Println(">>> port : ", port)
+			}
+		}
+	})
+
 }
